d4484a441ce0ecbba521d3e8c0d4d9ce88156a8c
João fernando <93411745+AoruFernando01@users.noreply.github.com>
2022-02-07 19:14:53 -0300

    Add files via upload
    Projeto finalizado.

---
+++ FORUUM/src/main/java/br/com/jf/forum/repository/CursoRepository.java
@@ -0,0 +1,10 @@
+package br.com.jf.forum.repository;
+
+import br.com.jf.forum.modelo.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+
+	Curso findByNome(String nome);
+
+}
